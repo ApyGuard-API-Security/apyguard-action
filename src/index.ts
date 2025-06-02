38e8fdb3c175031fb63733ed32d5c62639ebf4ac
anilyuk <anilyuk@users.noreply.github.com>
2025-06-02 19:49:30 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ async function run() {
     const startResponse = await client.post(`api/api_security/start_github_api_scan`, body);
     
     if (startResponse.status !== 200) {
-      throw new Error('Failed to start scan');
+      throw new Error(`Failed to start scan: ${startResponse.status} ${startResponse.statusText}`);
     }
 
     // Poll for results
